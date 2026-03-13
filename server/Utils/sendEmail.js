84183637e425d50fe17c67274d9a4f304e577dce
Prasad-web-creator <magendraprasad84@gmail.com>
2026-03-13 12:45:37 +0530

    server email changes 1

--- server/Utils/sendEmail.js
+++ server/Utils/sendEmail.js
@@ -12,6 +12,9 @@ const sendEmail = async (to, subject, text) => {
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
+      },
+      tls:{
+        rejectUnauthorized: false
       }
     })
 
